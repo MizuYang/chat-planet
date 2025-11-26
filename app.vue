eb9d63f15c15ade0ccfd1e39626ed46a8a112f36
mizu <psdk55052004@gmail.com>
2025-11-27 01:30:27 +0800

    feat(ui): 設定 Nuxt UI 與 Tailwind CSS 環境
    - 新增 .prettierrc 配置 Tailwind CSS 插件
    - 更新 VSCode 設定以支援 Tailwind CSS 自動完成
    - 重構 app.vue 結構並加入 Nuxt UI 元件
    - 新增主要樣式檔案引入 Tailwind CSS 與 Nuxt UI
    - 整合 @nuxt/ui 模組至專案配置
    - 新增首頁頁面

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <UApp>
+    <UColorModeButton />
+
+    <NuxtPage />
+
+    <div class="bg-white py-24">
+      <div class="flex flex-col items-center">
+        <h1 class="text-6xl font-semibold text-blue-600">2022 iThome</h1>
+        <p class="mt-4 p-3 text-9xl font-bold text-gray-900">鐵人賽</p>
+        <p class="mt-4 bg-amber-400 p-3 text-9xl font-bold text-gray-900">
+          鐵人賽
+        </p>
+      </div>
+    </div>
+  </UApp>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { CalendarDate } from "@internationalized/date";
+
+const value = shallowRef(new CalendarDate(2022, 2, 3));
+</script>
+
+<template>
+  <UCalendar v-model="value" />
+</template>
